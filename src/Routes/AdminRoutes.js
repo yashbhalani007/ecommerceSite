a9bb534ae491ecc8a4320396a21df5bcfd1aec57
KevalPolara <kevalpolara5272@gmail.com>
2023-12-15 17:42:53 +0530

    Keval_done

--- src/Routes/AdminRoutes.js
+++ src/Routes/AdminRoutes.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Dashboard from '../Admin/conatiner/Dashboard/Dashboard';
 import Content from '../Admin/conatiner/Content/Content';
 import Category from '../Admin/conatiner/Category/Category';
+import Order from '../Admin/conatiner/Order/Order';
 
 function AdminRoutes(props) {
   return (
@@ -11,10 +12,11 @@ function AdminRoutes(props) {
       <Routes>
           <Route path='/' element={<Dashboard />} />
           <Route path='managecontent' element={<Content />} />
+          <Route path='order' element={<Order />} />
           <Route path='category' element={<Category />} />
       </Routes>
     </LayOut>
   );
 }
 
-export default AdminRoutes;
+export default AdminRoutes; 
