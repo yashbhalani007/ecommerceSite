81c2b6755630659753995527020d90bd08441063
KevalPolara <kevalpolara5272@gmail.com>
2023-12-18 09:35:25 +0530

    keval_done

---
+++ src/Admin/conatiner/CatUpload/CatUpload.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function CatUpload(props) {
+    return (
+        <div>
+            <h1>This is a Proper Category form</h1>
+            <h1>This is a Proper Category form</h1>
+            <h1>This is a Proper Category form</h1>
+            <h1>This is a Proper Category  form</h1>
+            <h1>This is a Proper Category form</h1>
+            
+        </div>
+    );
+}
+
+export default CatUpload;
