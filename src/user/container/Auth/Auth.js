f22afca67fbe2fe4e13cdaf144c42adabef77b78
KevalPolara <kevalpolara5272@gmail.com>
2023-12-21 16:15:29 +0530

    keval_done

--- src/user/container/Auth/Auth.js
+++ src/user/container/Auth/Auth.js
@@ -1,113 +1,314 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, NavLink } from "react-router-dom";
+import * as yup from "yup";
+import { useFormik } from "formik";
+
 
 function Auth(props) {
-    return (
-        <div id='app'>
-            <div>
-                <div className="page-style-a">
-                    <div className="container">
-                        <div className="page-intro">
-                            <h2>Account</h2>
-                            <ul className="bread-crumb">
-                                <li className="has-separator">
-                                    <i className="ion ion-md-home" />
-                                    <a href="home.html">Home</a>
-                                </li>
-                                <li className="is-marked">
-                                    <a href="account.html">Account</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                {/* Page Introduction Wrapper /- */}
-                {/* Account-Page */}
-                <div className="page-account u-s-p-t-80">
-                    <div className="container">
-                        <div className="row">
-                            {/* Login */}
-                            <div className="col-lg-6">
-                                <div className="login-wrapper">
-                                    <h2 className="account-h2 u-s-m-b-20">Login</h2>
-                                    <h6 className="account-h6 u-s-m-b-30">Welcome back! Sign in to your account.</h6>
-                                    <form>
-                                        <div className="u-s-m-b-30">
-                                            <label htmlFor="user-name-email">Username or Email
-                                                <span className="astk">*</span>
-                                            </label>
-                                            <input type="text" id="user-name-email" className="text-field" placeholder="Username / Email" />
-                                        </div>
-                                        <div className="u-s-m-b-30">
-                                            <label htmlFor="login-password">Password
-                                                <span className="astk">*</span>
-                                            </label>
-                                            <input type="text" id="login-password" className="text-field" placeholder="Password" />
-                                        </div>
-                                        <div className="group-inline u-s-m-b-30">
-                                            <div className="group-1">
-                                                <input type="checkbox" className="check-box" id="remember-me-token" />
-                                                <label className="label-text" htmlFor="remember-me-token">Remember me</label>
-                                            </div>
-                                            <div className="group-2 text-right">
-                                                <div className="page-anchor">
-                                                    {/* <a href="lost-password.html">
-                                                        <i className="fas fa-circle-o-notch u-s-m-r-9" />Lost your password?</a> */}
-                                                        <NavLink className="fas fa-circle-o-notch u-s-m-r-9" to={"/forgotpassword"}>Lost your password?</NavLink>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="m-b-45">
-                                            <button className="button button-outline-secondary w-100">Login</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            {/* Login /- */}
-                            {/* Register */}
-                            <div className="col-lg-6">
-                                <div className="reg-wrapper">
-                                    <h2 className="account-h2 u-s-m-b-20">Register</h2>
-                                    <h6 className="account-h6 u-s-m-b-30">Registering for this site allows you to access your order status and history.</h6>
-                                    <form>
-                                        <div className="u-s-m-b-30">
-                                            <label htmlFor="user-name">Username
-                                                <span className="astk">*</span>
-                                            </label>
-                                            <input type="text" id="user-name" className="text-field" placeholder="Username" />
-                                        </div>
-                                        <div className="u-s-m-b-30">
-                                            <label htmlFor="email">Email
-                                                <span className="astk">*</span>
-                                            </label>
-                                            <input type="text" id="email" className="text-field" placeholder="Email" />
-                                        </div>
-                                        <div className="u-s-m-b-30">
-                                            <label htmlFor="password">Password
-                                                <span className="astk">*</span>
-                                            </label>
-                                            <input type="text" id="password" className="text-field" placeholder="Password" />
-                                        </div>
-                                        <div className="u-s-m-b-30">
-                                            <input type="checkbox" className="check-box" id="accept" />
-                                            <label className="label-text no-color" htmlFor="accept">I’ve read and accept the
-                                                <a href="terms-and-conditions.html" className="u-c-brand">terms &amp; conditions</a>
-                                            </label>
-                                        </div>
-                                        <div className="u-s-m-b-45">
-                                            <button className="button button-primary w-100">Register</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            {/* Register /- */}
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const [formName, setFormName] = useState("sign-up");
+  const dispatch = useDispatch();
+  console.log(formName);
 
+  const handleSingup = () => {
+
+  }
+
+  const handleLogin = () => {
+
+  }
+
+  const handlefPassword = () => {
+
+  }
+
+
+  let authobj, initialValues;
+
+  if (formName === "login") {
+    authobj = {
+      email: yup.string().email('Please Enter a Valid Email').required('Please Enter a Email'),
+      password: yup.string().required('Please Enter a Password')
+    };
+
+    initialValues = {
+      email: "",
+      password: ""
+    };
+  } else if (formName === "sign-up") {
+    authobj = {
+      email: yup.string().email('Please Enter a Valid Email').required('Please Enter a Email'),
+      phone: yup.string().required(),
+      name: yup.string().required('Please Enter a Valid Name'),
+      password :yup.string().required('Please Enter a Valid Password')
+    };
+
+    initialValues = {
+      email: "",
+      phone: "",
+      name: "",
+      password : "",
+      con_phone:""
+    };
+  } else if (formName === "fpassword") {
+    authobj = {
+      email: yup.string().email('Please Enter a valid Email').required('Please Enter a Email')
+    };
+
+    initialValues = {
+      email: ""
+    };
+  }
+
+
+  const authSchema = yup.object().shape(authobj);
+
+  const {
+    values,
+    errors,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    touched
+  } = useFormik({
+    initialValues: initialValues,
+    validationSchema: authSchema,
+    enableReinitialize: true,
+    onSubmit: (values, action) => {
+
+      if(formName=== "signUp"){
+        handleSingup(values);
+      }else if(formName === "login"){
+        handleLogin(values);
+      }else if(formName === "fpassword"){
+        handlefPassword(values);
+      }
+      console.log(values);
+      action.resetForm();
+    }
+  });
+
+  return (
+    <div id="app">
+      <div>
+        <div className="page-style-a">
+          <div className="container">
+            <div className="page-intro">
+              <h2>Account</h2>
+              <ul className="bread-crumb">
+                <li className="has-separator">
+                  <i className="ion ion-md-home" />
+                  <a href="home.html">Home</a>
+                </li>
+                <li className="is-marked">
+                  <a href="account.html">Account</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-    );
+        {/* Page Introduction Wrapper /- */}
+        {/* Account-Page */}
+        <div className="page-account u-s-p-t-80">
+          <div className="container">
+            <div className="row">      
+              <div className="col-lg-12">
+                <div className="reg-wrapper">
+               
+                    {formName === 'fpassword' ?   <h2 className="account-h2 u-s-m-b-20">Forgot Password</h2>:
+                    formName === 'login' ?  <h2 className="account-h2 u-s-m-b-20">Login</h2> :
+                    <h2 className="account-h2 u-s-m-b-20">Sign-Up</h2>
+                    } 
+                  <h6 className="account-h6 u-s-m-b-30">
+                    Registering for this site allows you to access your order
+                    status and history.
+                  </h6>
+                  <form onSubmit={handleSubmit}>
+                      <div className="u-s-m-b-30">
+                      
+                        {formName === 'sign-up' ?
+                             <>
+                         <label htmlFor="user-name">
+                           Username
+                           <span className="astk">*</span>
+                         </label>
+                         
+                         <input
+                           type="text"
+                           id="user-name"
+                           name="name"
+                           className="text-field"
+                           placeholder="Username"
+                           onChange={handleChange}
+                           onBlur={handleBlur}
+                           value={values.name}
+                         />
+                         
+                     {touched.name && errors.name
+                      ? <div className="error-handle">
+                          {errors.name}
+                        </div>
+                      : null} 
+                         </> : ''
+                        }
+                         
+                       </div>
+                 
+                    <div className="u-s-m-b-30">
+                      <label htmlFor="email">
+                        Email
+                        <span className="astk">*</span>
+                      </label>
+                      <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        className="text-field"
+                        placeholder="Email"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                      />
+
+                        {touched.email && errors.email
+                      ? <div className="error-handle">
+                          {errors.email}
+                        </div>
+                      : null}
+
+
+                    </div>
+                    <div className="u-s-m-b-30">
+                        {
+                            formName === 'fpassword' ? '' :
+                            <>
+                            <label htmlFor="password">
+                            Password
+                            <span className="astk">*</span>
+                          </label>
+                          <input
+                            type="text"
+                            id="password"
+                            name="password"
+                            className="text-field"
+                            placeholder="Password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                          />
+
+                        {touched.password && errors.password
+                      ? <div className="error-handle">
+                          {errors.password}
+                        </div>
+                      : null}
+
+
+                          </>
+                        }
+                      
+                    </div>
+                    <div className="u-s-m-b-30">
+                      <input
+                        type="checkbox"
+                        className="check-box"
+                        id="accept"
+                      />
+                      <label className="label-text no-color" htmlFor="accept">
+                        I’ve read and accept the
+                        <a
+                          href="terms-and-conditions.html"
+                          className="u-c-brand"
+                        >
+                          terms &amp; conditions
+                        </a>
+                      </label>
+                    </div>
+
+                    <div className="m-b-45">
+                      {formName === "sign-up" ? (
+                        <div className="formsignup">
+                        
+                          <Link>
+                            <a
+                                className="clickhere"
+                              onClick={() => {
+                                setFormName("login");
+                            
+                              }}
+                            >
+                               <p className="login">Already Have An Account ?</p>
+                            </a>
+                          </Link>
+                        </div>
+                      ) : formName === "login" ? (
+                        <>                        <div className="formsignup">
+
+                          <a
+                           className="clickhere"
+                            onClick={() => {
+                              setFormName("sign-up");
+                            }}
+                          >
+                        <p className="login">Create An Account?</p>
+
+                          </a>
+                          </div>
+                          <div className="formsignup">
+
+                            <a
+                             className="clickhere"
+                              onClick={() => {
+                                setFormName("fpassword");
+                              }}
+                            >
+                            <p className="login">Forgot Password ?</p>
+
+                            </a>
+                         
+                        </div>
+                        </>
+
+                      ) : (
+                        <div className="formsignup">
+                          <Link>
+                            <a
+                             className="clickhere"
+                              onClick={() => {
+                                setFormName("login");
+                              }}><p className="login">Back to Login</p>
+                              </a>
+                          </Link>
+                        </div>
+                      )}
+                    </div>
+                    <div className="m-b-45">
+                      {formName === "fpassword" ? (
+                        <button className="button button-primary w-100">
+                          Reset Password
+                        </button>
+                      ) : formName === "login" ? (
+                        <div>
+                          <button className="button button-primary w-100">
+                            Login
+                          </button>
+                        </div>
+                      ) : (
+                        <button className="button button-primary w-100">
+                          Register
+                        </button>
+                      )}
+                    </div>
+                  </form>
+                </div>
+              </div>
+              {/* Register /- */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
