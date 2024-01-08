74d1c952c1227ea15c2f1f56b6d1025be5e8f504
srushti26794 <srushtichangela267@gmail.com>
2024-01-08 17:38:08 +0530

    Done

--- src/Admin/conatiner/CatUpload/RenderCatlog.js
+++ src/Admin/conatiner/CatUpload/RenderCatlog.js
@@ -11,6 +11,7 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import AddCatelog from './AddCatelog';
+import { useNavigate } from 'react-router';
 
 function RenderCatlog(props) {
     const [fileInputs, setFileInputs] = useState([{ id: 1, selectedFile: null }]);
